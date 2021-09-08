ef3e0edd32abbf117ba20062d3bb916f2897e2c7
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-09-08 10:35:33 +0200

    fix route('comic') classe active

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,16 +8,16 @@
         </div>
         <nav>
             <ul>
-                <li><a class="{{ ('http://127.0.0.1:8000/' . Route::currentRouteName() == route('characters')) ? 'active' : '' }}" href="{{ route('characters') }}">characters</a></li>
-                <li><a class="{{ ('http://127.0.0.1:8000/' . Route::currentRouteName() == route('comics')) ? 'active' : '' }}" href="{{ route('comics') }}">comics</a></li>
-                <li><a class="{{ ('http://127.0.0.1:8000/' . Route::currentRouteName() == route('movie')) ? 'active' : '' }}" href="{{ route('movie') }}">movies</a></li>
-                <li><a class="{{ ('http://127.0.0.1:8000/' . Route::currentRouteName() == route('tv')) ? 'active' : '' }}" href="{{ route('tv') }}">tv</a></li>
-                <li><a class="{{ ('http://127.0.0.1:8000/' . Route::currentRouteName() == route('game')) ? 'active' : '' }}" href="{{ route('game') }}">games</a></li>
-                <li><a class="{{ ('http://127.0.0.1:8000/' . Route::currentRouteName() == route('collectibles')) ? 'active' : '' }}" href="{{ route('collectibles') }}">collectibles</a></li>
-                <li><a class="{{ ('http://127.0.0.1:8000/' . Route::currentRouteName() == route('videos')) ? 'active' : '' }}" href="{{ route('videos') }}">videos</a></li>
-                <li><a class="{{ ('http://127.0.0.1:8000/' . Route::currentRouteName() == route('fans')) ? 'active' : '' }}" href="{{ route('fans') }}">fans</a></li>
-                <li><a class="{{ ('http://127.0.0.1:8000/' . Route::currentRouteName() == route('news')) ? 'active' : '' }}" href="{{ route('news') }}">news</a></li>
-                <li><a class="{{ ('http://127.0.0.1:8000/' . Route::currentRouteName() == route('shop')) ? 'active' : '' }}" href="{{ route('shop') }}">shop</a></li>
+                <li><a class="{{ (Request::url() == route('characters')) ? 'active' : '' }}" href="{{ route('characters') }}">characters</a></li>
+                <li><a class="{{ (Request::url() == route('comics')) ? 'active' : '' }}" href="{{ route('comics') }}">comics</a></li>
+                <li><a class="{{ (Request::url() == route('movie')) ? 'active' : '' }}" href="{{ route('movie') }}">movies</a></li>
+                <li><a class="{{ (Request::url() == route('tv')) ? 'active' : '' }}" href="{{ route('tv') }}">tv</a></li>
+                <li><a class="{{ (Request::url() == route('game')) ? 'active' : '' }}" href="{{ route('game') }}">games</a></li>
+                <li><a class="{{ (Request::url() == route('collectibles')) ? 'active' : '' }}" href="{{ route('collectibles') }}">collectibles</a></li>
+                <li><a class="{{ (Request::url() == route('videos')) ? 'active' : '' }}" href="{{ route('videos') }}">videos</a></li>
+                <li><a class="{{ (Request::url() == route('fans')) ? 'active' : '' }}" href="{{ route('fans') }}">fans</a></li>
+                <li><a class="{{ (Request::url() == route('news')) ? 'active' : '' }}" href="{{ route('news') }}">news</a></li>
+                <li><a class="{{ (Request::url() == route('shop')) ? 'active' : '' }}" href="{{ route('shop') }}">shop</a></li>
 
             </ul>
         </nav>
