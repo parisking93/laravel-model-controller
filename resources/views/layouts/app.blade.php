10b0c4e890d8b9d9bc605f80fc4db632528fadb4
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-09-08 10:16:20 +0200

    add link active quando accedo alla pagina

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,15 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{ asset('css/app.css')}} ">
+        <!-- prendo il nome della route in cui mi trovo -->
+        <title>{{ Route::currentRouteName()}}</title>
 
-        <title>{{ Request::route()->getName()}}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
     </head>
     <body>
-            
+        
         @include('partials.header')
         <main> @yield('main')</main>
         @include('partials.footer')
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,16 +8,17 @@
         </div>
         <nav>
             <ul>
-                <li><a href="{{ route('characters') }}">characters</a></li>
-                <li><a href="{{ route('comics') }}">comics</a></li>
-                <li><a href="{{ route('movie') }}">movies</a></li>
-                <li><a href="{{ route('tv') }}">tv</a></li>
-                <li><a href="{{ route('game') }}">games</a></li>
-                <li><a href="{{ route('collectibles') }}">collectibles</a></li>
-                <li><a href="{{ route('videos') }}">videos</a></li>
-                <li><a href="{{ route('fans') }}">fans</a></li>
-                <li><a href="{{ route('news') }}">news</a></li>
-                <li><a href="{{ route('shop') }}">shop</a></li>
+                <li><a class="{{ ('http://127.0.0.1:8000/' . Route::currentRouteName() == route('characters')) ? 'active' : '' }}" href="{{ route('characters') }}">characters</a></li>
+                <li><a class="{{ ('http://127.0.0.1:8000/' . Route::currentRouteName() == route('comics')) ? 'active' : '' }}" href="{{ route('comics') }}">comics</a></li>
+                <li><a class="{{ ('http://127.0.0.1:8000/' . Route::currentRouteName() == route('movie')) ? 'active' : '' }}" href="{{ route('movie') }}">movies</a></li>
+                <li><a class="{{ ('http://127.0.0.1:8000/' . Route::currentRouteName() == route('tv')) ? 'active' : '' }}" href="{{ route('tv') }}">tv</a></li>
+                <li><a class="{{ ('http://127.0.0.1:8000/' . Route::currentRouteName() == route('game')) ? 'active' : '' }}" href="{{ route('game') }}">games</a></li>
+                <li><a class="{{ ('http://127.0.0.1:8000/' . Route::currentRouteName() == route('collectibles')) ? 'active' : '' }}" href="{{ route('collectibles') }}">collectibles</a></li>
+                <li><a class="{{ ('http://127.0.0.1:8000/' . Route::currentRouteName() == route('videos')) ? 'active' : '' }}" href="{{ route('videos') }}">videos</a></li>
+                <li><a class="{{ ('http://127.0.0.1:8000/' . Route::currentRouteName() == route('fans')) ? 'active' : '' }}" href="{{ route('fans') }}">fans</a></li>
+                <li><a class="{{ ('http://127.0.0.1:8000/' . Route::currentRouteName() == route('news')) ? 'active' : '' }}" href="{{ route('news') }}">news</a></li>
+                <li><a class="{{ ('http://127.0.0.1:8000/' . Route::currentRouteName() == route('shop')) ? 'active' : '' }}" href="{{ route('shop') }}">shop</a></li>
+
             </ul>
         </nav>
         <div class="box-search-bar">
